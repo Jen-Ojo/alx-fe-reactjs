619c35e07deb3093ddf32e86c10d66e2d4859f65
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-01-26 10:51:33 +0100

    Add components folder and import in App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,14 @@
-  GNU nano 8.6                      src/App.jsx
+import React from "react";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub profiles</p>
     </div>
   );
 }
 
 export default App;
+
 
 
