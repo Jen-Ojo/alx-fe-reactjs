2f525e450db7b2d9e0de52ad0a13ff1aaa42094f
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-01-26 11:24:04 +0100

    Fix app structure for checker

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div>
-      <h1>GitHub User Search</h1>
-    </div>
-  );
+  return <div>GitHub User Search</div>;
 }
 
 export default App;
-
-
-
