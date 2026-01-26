cfde4f25980a0df9aa67b8cfdd025158fd05b2cc
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-01-26 15:44:06 +0100

    Add app structure and environment setup

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,17 @@
-  GNU nano 8.6                                                                    src/App.jsx
 import React from "react";
 
 function App() {
-  return <div>GitHub User Search App</div>;
+  return (
+    <div>
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+
+      <main>
+        <p>Search for GitHub users</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,10 +1,5 @@
-import axios from 'axios';
+const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-const githubApi = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: {
-    Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
-  },
-});
-
-export default githubApi;
+export const searchUsers = async (query) => {
+  return [];
+};
