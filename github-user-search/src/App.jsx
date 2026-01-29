8a55380850abab5c1a83cc1c5b007a231367e6b3
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-01-29 14:05:08 +0100

    Fix basic application structure by adding imports

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
+import Search from "./components/Search";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        GitHub User Search
-      </h1>
+    <div className="min-h-screen bg-gray-100">
+      <Search />
     </div>
   );
 }
