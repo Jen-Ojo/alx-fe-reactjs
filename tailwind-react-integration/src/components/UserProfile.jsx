d96574d8d56e92407315748820e0c56d6880fe45
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-02-05 20:26:15 +0100

    Add hover effects and transitions to UserProfile with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,17 +1,18 @@
 function UserProfile() {
   return (
-    <div className="
-      user-profile
-      
-      bg-gray-100
-      p-4 md:p-8
-      max-w-xs md:max-w-sm
-      mx-auto my-20
-      rounded-lg
-      shadow-lg
-    ">
-        <div className="sm:p-4">
-            "sm:w-24", "sm:h-24", "sm:mx-0"
+    <div
+      className="
+        user-profile
+        bg-gray-100
+        p-4 md:p-8
+        max-w-xs md:max-w-sm
+        mx-auto my-20
+        rounded-lg
+        shadow-lg
+        transition-shadow duration-300 ease-in-out
+        hover:shadow-xl
+      "
+    >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
@@ -20,23 +21,25 @@ function UserProfile() {
           w-24 h-24
           md:w-36 md:h-36
           mx-auto
+          transition-transform duration-300 ease-in-out
+          hover:scale-110
         "
       />
 
-      <h1 className="
-        text-lg md:text-xl
-        text-blue-800
-        my-4
-        text-center
-      ">
+      <h1
+        className="
+          text-lg md:text-xl
+          text-blue-800
+          my-4
+          text-center
+          transition-colors duration-300 ease-in-out
+          hover:text-blue-500
+        "
+      >
         John Doe
       </h1>
 
-      <p className="
-        text-gray-600
-        text-sm md:text-base
-        text-center
-      ">
+      <p className="text-gray-600 text-sm md:text-base text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
@@ -44,4 +47,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
