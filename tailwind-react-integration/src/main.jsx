4bb48e2ca0c9b9d519941b30add1e825a1ec025b
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-02-05 16:09:29 +0100

    Add UserProfile component styled with Tailwind CSS

--- tailwind-react-integration/src/main.jsx
+++ tailwind-react-integration/src/main.jsx
@@ -1,11 +1,11 @@
-import './index.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import UserProfile from "./components/UserProfile";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
