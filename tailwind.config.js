c6750eb4514e6be8977dd0dfffea1a79ce49d6f9
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-02-14 22:25:42 +0100

    Set up HomePage with mock recipe data and Tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ contain: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"] ,
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
