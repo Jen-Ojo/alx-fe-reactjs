f5a375a72d1e1cda5d3da5f20cce6f734da9151f
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-02-15 15:17:02 +0100

    Remove submodule and add recipe-sharing-platform as regular directory

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ recipe-sharing-platform/src/pages/RecipeDetail.jsx
@@ -0,0 +1,9 @@
+function RecipeDetail() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Recipe Detail</h1>
+    </div>
+  );
+}
+
+export default RecipeDetail;
