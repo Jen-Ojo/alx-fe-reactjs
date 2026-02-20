cfd1cd0ea4f4cbe52966d9f365736712653b5ca4
Jenninfr Ojo <omorogbejennifer35@gmail.com>
2026-02-20 08:00:10 +0100

    Complete form handling project with controlled and formik forms

--- form-handling-react/src/pages/RecipeDetail.jsx
+++
@@ -1,9 +0,0 @@
-function RecipeDetail() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Recipe Detail</h1>
-    </div>
-  );
-}
-
-export default RecipeDetail;
--- form-handling-react/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
- contain: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"] ,
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
